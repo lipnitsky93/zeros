61914f11e598b22344de3d56c5a27ced1fcb6d22
lipnitsky93 <lipnitsky93@yandex.ru>
2018-02-22 12:39:45 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var commonCount = 0;
+  var n = 5;
+  while (n < number) {
+  var count = Math.floor(number / n);
+  n *= 5;
+  
+  commonCount += count;
+  }
+  return commonCount;
 }
